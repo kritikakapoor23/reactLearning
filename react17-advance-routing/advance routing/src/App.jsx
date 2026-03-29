6a411095aed53879d13e9af948ac9bac859541c1
Kritika Kapoor <kritikakapoor131@gmail.com>
2026-03-29 17:47:24 +0530

    advance routing

---
+++ react17-advance-routing/advance routing/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import About from './pages/About'
+import { Route, Routes } from 'react-router-dom'
+import NotFound from './pages/NotFound'
+import Men from './pages/Men'
+import Women from './pages/Women'
+import Kids from './pages/Kids'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black text-white'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product' element={<Product />} >
+          <Route path='men' element={<Men />} />
+          <Route path='women' element={<Women />} />
+          <Route path='kids' element={<Kids />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
